87df1227922ad4f81e96219b348b8b73685fe43c
Joona Kytöniemi <jnkyto@utu.fi>
2025-06-17 15:25:03 +0300

    Add scala for testing

---
+++ lm_eval/tasks/finbench_v2/scala_fi_utils.py
@@ -0,0 +1,17 @@
+import datasets
+
+def process_docs(dataset: datasets.Dataset) -> datasets.Dataset:
+    def _process_doc(doc):
+        label_map = {
+            "correct": "kyllä",
+            "incorrect": "ei"
+        }
+
+        out_doc = {
+            "query": f"Lause: {doc["text"]}\nKieliopillisesti oikein: ",
+            "choices": [label_map["correct"], label_map["incorrect"]],
+            "gold": label_map[doc["label"]]
+        }
+        return out_doc
+
+    return dataset.map(_process_doc)
