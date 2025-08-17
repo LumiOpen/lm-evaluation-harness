cb3ef61870adb3179feaa0909d8e33ee70460cc9
Joona Kytöniemi <jnkyto@utu.fi>
2025-08-17 21:22:14 +0300

    Add Goldenswag and TruthfulQA multiprompt

--- lm_eval/tasks/finbench_v2/truthfulqa/gen/ogx_truthfulqax_utils.py
+++ lm_eval/tasks/finbench_v2/truthfulqa/gen/ogx_truthfulqax_utils.py
@@ -37,8 +37,8 @@ def preprocess_function(examples):
 
     incorrect_answers = _format_answers(examples["incorrect_answers"])
     correct_answers = _format_answers(examples["correct_answers"])
-    if "I have no comment." not in correct_answers:
-        correct_answers.append("I have no comment.")
+    if "En kommentoi." not in correct_answers:
+        correct_answers.append("En kommentoi.")
     return {
         "question": examples["question"].strip(),
         "correct_answers": correct_answers,
