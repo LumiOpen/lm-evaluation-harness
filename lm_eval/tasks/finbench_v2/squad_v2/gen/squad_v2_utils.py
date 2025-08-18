14de05685b9f450e14f83532087653d8aadaa0eb
Joona Kytöniemi <jnkyto@utu.fi>
2025-08-18 15:52:00 +0300

    Redo SQuAD v2 task defs and update groups

---
+++ lm_eval/tasks/finbench_v2/squad_v2/gen/squad_v2_utils.py
@@ -0,0 +1,64 @@
+# Based on NorEval - https://github.com/EleutherAI/lm-evaluation-harness/tree/main/lm_eval/tasks/noreval
+
+import datasets
+import transformers.data.metrics.squad_metrics as squad_metrics
+
+
+def process_results(doc, results):
+    preds = results[0]
+    reference = doc["answers"]["text"][0]
+    f1_sum = squad_metrics.compute_f1(reference, preds)
+    exact_match = squad_metrics.compute_exact(reference, preds)
+    return {"f1": f1_sum, "exact_match": exact_match}
+
+
+def process_docs(dataset: datasets.Dataset):
+    def _helper(doc):
+        doc["title"] = doc["context"].strip().split("\n")[0].strip()
+        doc["passage"] = "\n".join(doc["context"].strip().split("\n")[1:]).strip()
+        doc["question"] = " ".join(doc["question"].strip().split())
+        return doc
+
+    return dataset.map(_helper)
+
+
+def p1(doc):
+    title = doc["title"]
+    passage = doc["passage"]
+    question = doc["question"]
+    prompt = f"Otsikko: {title}\n\nTeksti: {passage}\n\nKysymys: {question}\nVastaus:"
+    return prompt
+
+
+def p2(doc):
+    title = doc["title"]
+    passage = doc["passage"]
+    question = doc["question"]
+    prompt = f"Vastaa kysymykseen seuraavan tekstin perusteella.\nAihe: {title}\nTeksti: {passage}\n\nKysymys: {question}\nVastauksesi:"
+    return prompt
+
+
+def p3(doc):
+    title = doc["title"]
+    passage = doc["passage"]
+    question = doc["question"]
+    prompt = (
+        f"Lue seuraava teksti ja vastaa kysymykseen. Aihe: {title}\nTeksti: {passage}\nKysymys: {question}\nVastaus:"
+    )
+    return prompt
+
+
+def p4(doc):
+    title = doc["title"]
+    passage = doc["passage"]
+    question = doc["question"]
+    prompt = f'Tässä on teksti aiheesta: "{title}":\n{passage}\n\nVastaa seuraavaan kysymykseen tekstin perusteella: {question}\nVastaus:'
+    return prompt
+
+
+def p5(doc):
+    title = doc["title"]
+    passage = doc["passage"]
+    question = doc["question"]
+    prompt = f'Aineisto:\n\n{title}\n{passage}\n\nVastaa aineiston perusteella kysymykseen: "{question}"\n\nVastaus:'
+    return prompt
