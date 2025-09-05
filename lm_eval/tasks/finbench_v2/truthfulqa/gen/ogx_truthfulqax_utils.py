ebadc8da101664fe2af141dea87bf8e4f0b3a455
Joona Kytöniemi <jnkyto@utu.fi>
2025-09-05 16:33:15 +0300

    Update README and remove obsolete code from TQA variants

--- lm_eval/tasks/finbench_v2/truthfulqa/gen/ogx_truthfulqax_utils.py
+++ lm_eval/tasks/finbench_v2/truthfulqa/gen/ogx_truthfulqax_utils.py
@@ -5,22 +5,8 @@ import numpy as np
 from rouge_score import rouge_scorer, scoring
 
 
-def process_results_mc2(doc, results):
-    lls, is_greedy = zip(*results)
-
-    # Split on the first `0` as everything before it is true (`1`).
-    split_idx = list(doc["mc2_targets"]["labels"]).index(0)
-    # Compute the normalized probability mass for the correct answer.
-    ll_true, ll_false = lls[:split_idx], lls[split_idx:]
-    p_true, p_false = np.exp(np.array(ll_true)), np.exp(np.array(ll_false))
-    p_true = p_true / (sum(p_true) + sum(p_false))
-
-    return {"acc": sum(p_true)}
-
-
 def process_docs_gen(dataset: datasets.Dataset) -> datasets.Dataset:
     return dataset.map(preprocess_function)
-
 
 def preprocess_function(examples):
     def _format_answers(answers):
